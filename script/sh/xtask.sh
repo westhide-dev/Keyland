37e0397e50b08f39406413e4897bcb9ddf4845b7
westhide.dev <westhide.dev@gmail.com>
2024-04-08 22:53:04 +0800

    chore: cargo tree

--- script/sh/xtask.sh
+++ script/sh/xtask.sh
@@ -5,6 +5,9 @@ set -ex
 CURRENT_DIR=$(dirname $0)
 KEYLAND_DIR=$(cd ${CURRENT_DIR}/../../ & pwd)
 
+# Deps
+cargo tree
+
 # Check
 cargo check  --verbose    --color always --all-targets --all-features
 
