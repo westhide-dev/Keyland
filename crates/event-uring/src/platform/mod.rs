b59384421d5c4e04ef75456651d61c96a1482e89
westhide.dev <westhide.dev@gmail.com>
2024-04-19 22:36:26 +0800

    fix: lints.clippy unused_attributes

--- crates/event-uring/src/platform/mod.rs
+++ crates/event-uring/src/platform/mod.rs
@@ -7,9 +7,4 @@ pub mod macos;
 #[cfg(feature = "platform_webos")]
 pub mod webos;
 
-#[cfg_attr(feature = "platform_linux", path = "linux/mod.rs")]
-#[cfg_attr(feature = "platform_macos", path = "macos/mod.rs")]
-#[cfg_attr(feature = "platform_webos", path = "webos/mod.rs")]
-pub mod __implement__;
-
 pub trait Platform {}
